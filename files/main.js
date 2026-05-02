19be3c49ca781f59d1e8abd50eb8b5ce58d8dd49
uiscv545 <coraspeidel2020@gmail.com>
2026-05-02 03:53:15 +0200

    Update main.js

--- files/main.js
+++ files/main.js
@@ -4,7 +4,6 @@ import { pipeline } from "node:stream/promises";
 export const config = {
   api: { bodyParser: false },
   supportsResponseStreaming: true,
-  maxDuration: 60,
 };
 
 const T_B = (process.env.tar_d || "").replace(/\/$/, "");
